f3a2e566b73f19ba8f665b11d96cb3a7af2890c8
Ilham Prabaswara <ilhamprabaswara@gmail.com>
2023-03-17 19:03:27 +0700

    Simple to-do list form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 
+function CheckBox(props) {
+  return (
+    <>
+      <input type="checkbox" name={props.name} value={props.name}></input>
+      <label for="git_cli"> {props.label}</label>
+    </>
+  )
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src='logo.png' className="App-logo" alt="logo" />
       </header>
+      <main>
+        <h1> THINGS TO DO </h1>
+        <p>During bootcamp in sanbercode</p>
+        <hr />
+        <CheckBox name="git_cli" label="Belajar GIT & CLI" />
+        <hr />
+        <CheckBox name="html_css" label="Belajar HTML & CSS" />
+        <hr />
+        <CheckBox name="js" label="Belajar JavaScript" />
+        <hr />
+        <CheckBox name="react_basic" label="Belajar ReactJS Dasar" />
+        <hr />
+        <CheckBox name="react_advance" label="Belajar ReactJS Advance" />
+      </main>
     </div>
   );
 }
