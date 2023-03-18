bcc0d44629c0001e191bb540a978c9d385a4b30e
Ilham Prabaswara <ilhamprabaswara@gmail.com>
2023-03-18 23:12:01 +0700

    Updating CRUD

--- src/components/crud.js
+++ src/components/crud.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 
 var daftarBuah = [
     { nama: "Nanas", hargaTotal: 100000, beratTotal: 4000 },
@@ -9,10 +9,27 @@ var daftarBuah = [
 ]
 
 export default function CRUD() {
+    const [listBuah, setListBuah] = useState(daftarBuah);
+    const [inputNamaBuah, setInputNamaBuah] = useState("");
+    const [inputHargaBuah, setInputHargaBuah] = useState(0);
+    const [inputBeratBuah, setInputBeratBuah] = useState(2000);
+    const handleNama = (e) => {
+        setInputNamaBuah(e.target.value)
+    }
+    const handleHarga = (e) => {
+        setInputHargaBuah(e.target.value)
+    }
+    const handleBerat = (e) => {
+        setInputBeratBuah(e.target.value)
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setListBuah([...listBuah, { nama: inputNamaBuah, hargaTotal: inputHargaBuah, beratTotal: inputBeratBuah }])
+    }
     return (
         <>
             <h1>Daftar Harga Buah</h1>
-            <table>
+            <table className="fruitTable">
                 <thead>
                     <tr>
                         <th>No</th>
@@ -25,7 +42,7 @@ export default function CRUD() {
                 </thead>
                 <tbody>
                     {
-                        daftarBuah.map((item, index) => {
+                        listBuah.map((item, index) => {
                             return (
                                 <tr key={index}>
                                     <td>{index + 1}</td>
@@ -43,6 +60,27 @@ export default function CRUD() {
                     }
                 </tbody>
             </table>
+            <h1>Form Daftar Harga Buah</h1>
+            <fieldset>
+                <form onSubmit={handleSubmit}>
+                    <div className="formField">
+                        <label for="nama">Nama:</label>
+                        <input required type="text" name="nama" value={inputNamaBuah} onChange={handleNama} />
+                    </div>
+                    <div className="formField">
+                        <label for="hargaTotal">Harga Total:</label>
+                        <input required type="number" name="hargaTotal" value={inputHargaBuah} onChange={handleHarga} />
+                    </div>
+                    <div className="formField">
+                        <label for="beratTotal">Berat Total (dalam gram):</label>
+                        <input required min="2000" type="number" name="beratTotal" value={inputBeratBuah} onChange={handleBerat} />
+                    </div>
+                    <div className="formField">
+                        <label></label>
+                        <button>Submit</button>
+                    </div>
+                </form>
+            </fieldset>
         </>
     )
 }
